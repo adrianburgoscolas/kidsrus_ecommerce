75866c021ba321a99b11fbe874e19ccadc9d8fc7
Adrian Burgos <adrian.burgos.colas@gmail.com>
2023-05-31 14:17:07 -0400

    FIX: changed the screenshot, accept cookies get hidded after been moved down

--- src/app/AcceptCookies.tsx
+++ src/app/AcceptCookies.tsx
@@ -2,17 +2,24 @@
 import { useEffect, useState } from 'react';
 
 export default function AcceptCookies() {
-  const [show, setShow] = useState(false);
+  const [style, setStyle] = useState({translate:true, hide:false});
+
   useEffect(() => {
-    const timeout = setTimeout(() => setShow(true),2000);
+    const timeout = setTimeout(() => setStyle({translate: false,hide: false}),2000);
     return () => {
       clearTimeout(timeout);
     };
   },[]);
+
+  function handleClick() {
+    setStyle({translate: true, hide: false});
+    setTimeout(() => setStyle({translate: true,hide: true}), 800);
+  }
+
   return (
-    <div className={`${!show && 'translate-y-20'} transition-all duration-700 p-2 fixed bottom-0 z-30 flex items-center justify-center bg-seclight left-0 w-[100vw]`}>
+    <div className={`${style.translate && 'translate-y-20'} ${style.hide && 'opacity-0'} transition-all duration-700 p-2 fixed bottom-0 z-30 flex items-center justify-center bg-seclight left-0 w-[100vw]`}>
       <p className="p-1">This website use cookies to improve your experience. We&apos;ll assume you&apos;r ok with this, but you can opt-out if you wish</p>
-      <button onClick={_ => setShow(false)} className='bg-black text-seclight p-1'>Accept</button>
+      <button onClick={handleClick} className='bg-black text-seclight p-1'>Accept</button>
     </div>
   );
 }
